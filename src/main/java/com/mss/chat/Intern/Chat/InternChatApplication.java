6932f2b51cfca0c5e01ab6505feb33fbc9d11ee5
Daoudi Zakaria <zakaria.daoudi2@uit.ac.ma>
2022-11-18 10:08:40 +0100

    initial commit

---
+++ src/main/java/com/mss/chat/Intern/Chat/InternChatApplication.java
@@ -0,0 +1,13 @@
+package com.mss.chat.Intern.Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mss/chat/Intern/Chat/InternChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss.chat.Intern.Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
